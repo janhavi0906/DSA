9f177ffd93f3bd37f27c9da702bba3b7fd77694b
janhavi0906 <143177147+janhavi0906@users.noreply.github.com>
2024-06-19 13:09:09 +0530

    Sum of Three numbers

---
+++ sum of 3 no.s
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=120;
+        int b=987;
+        int c=724;
+        int sum= a+b+c;
+        System.out.println("The sum of the numbers is:" +sum);
+    }
+}
